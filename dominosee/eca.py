bc3345f2a3185379c21fa921514f8d76dbff2ce3
hmwang@climate <hm_wang@outlook.com>
2025-05-01 22:55:25 +0800

    Fix output data format

--- dominosee/eca.py
+++ dominosee/eca.py
@@ -179,7 +179,7 @@ def get_eca_precursor(eventA_precursor_window: xr.DataArray, eventB: xr.DataArra
                                       output_core_dims=[[spatial_dimA[-1], spatial_dimB[-1]]],
                                       dask="parallelized",
                                       dask_gufunc_kwargs={"allow_rechunk": True},
-                                      output_dtypes=np.uint16,
+                                      output_dtypes=[np.uint16],
                                       )
     else:
         da_precursor = xr.apply_ufunc(_eca_precursor, eventA_precursor_window, eventB, 
@@ -210,7 +210,7 @@ def get_eca_trigger(eventA_trigger_window: xr.DataArray, eventB: xr.DataArray, f
                                       output_core_dims=[[spatial_dimA[-1], spatial_dimB[-1]]],
                                       dask="parallelized",
                                       dask_gufunc_kwargs={"allow_rechunk": True},
-                                      output_dtypes=np.uint16,
+                                      output_dtypes=[np.uint16],
                                       )
     else:
         da_trigger = xr.apply_ufunc(_eca_trigger, eventA_trigger_window, eventB, 
@@ -337,115 +337,115 @@ def get_eca_trigger_confidence(trigger: xr.DataArray, eventA: xr.DataArray, even
 """
 Legacy stack-style calculation
 """
-@njit(parallel=False)
-def eca(b1, b2, b1w, b2wr, dtype='uint16'):
-    # TODO: 拆分成precursor & trigger 两个函数；因为有时只需要一种
-    KRprec = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # precursor rates
-    KRtrig = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # triggering rates
-    for j in range(b1.shape[0]):
-        for k in range(b2.shape[0]): # TODO: 规范化代码时考虑只在这一层使用多核，对比一下速度；避免MPI，从而避免稀疏要求
-            KRprec[j, k] = np.sum(b2[k, :] & b1w[j, :])   # precursor: b1   => (b2)  
-            KRtrig[j, k] = np.sum(b1[j, :] & b2wr[k, :])  # trigger: (b1) => b2 
-    return KRprec, KRtrig
+# @njit(parallel=False)
+# def eca(b1, b2, b1w, b2wr, dtype='uint16'):
+#     # TODO: 拆分成precursor & trigger 两个函数；因为有时只需要一种
+#     KRprec = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # precursor rates
+#     KRtrig = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # triggering rates
+#     for j in range(b1.shape[0]):
+#         for k in range(b2.shape[0]): # TODO: 规范化代码时考虑只在这一层使用多核，对比一下速度；避免MPI，从而避免稀疏要求
+#             KRprec[j, k] = np.sum(b2[k, :] & b1w[j, :])   # precursor: b1   => (b2)  
+#             KRtrig[j, k] = np.sum(b1[j, :] & b2wr[k, :])  # trigger: (b1) => b2 
+#     return KRprec, KRtrig
 
 
-@njit(parallel=True)
-def eca_parallel(b1, b2, b1w, b2wr, dtype='uint16'):
-    KRprec = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # precursor rates
-    KRtrig = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # triggering rates
-    for j in prange(b1.shape[0]):
-        for k in range(b2.shape[0]): # TODO: 规范化代码时考虑只在这一层使用多核，对比一下速度；避免MPI，从而避免稀疏要求
-            KRprec[j, k] = np.sum(b2[k, :] & b1w[j, :])   # b1   => (b2)  
-            KRtrig[j, k] = np.sum(b1[j, :] & b2wr[k, :])  # (b1) => b2    
-    return KRprec, KRtrig
+# @njit(parallel=True)
+# def eca_parallel(b1, b2, b1w, b2wr, dtype='uint16'):
+#     KRprec = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # precursor rates
+#     KRtrig = np.zeros((b1.shape[0], b2.shape[0]), dtype=dtype)  # triggering rates
+#     for j in prange(b1.shape[0]):
+#         for k in range(b2.shape[0]): # TODO: 规范化代码时考虑只在这一层使用多核，对比一下速度；避免MPI，从而避免稀疏要求
+#             KRprec[j, k] = np.sum(b2[k, :] & b1w[j, :])   # b1   => (b2)  
+#             KRtrig[j, k] = np.sum(b1[j, :] & b2wr[k, :])  # (b1) => b2    
+#     return KRprec, KRtrig
 
 
-def eca_dataset(b1: xr.DataArray, b2: xr.DataArray, b1w: xr.DataArray, b2wr: xr.DataArray, dtype=None, parallel=True):
-    # TODO: 修正loc A/B的命名
-    # infer dtype based on the length of time dimension
-    if dtype is None:
-        dtype = np.uint8 if b1.shape[0] < 256 else np.uint16 if b1.shape[0] < 65536 else np.uint32  # Cannot use string here in numba
-    # get the location name from dims
-    xdim = np.setdiff1d(b1.dims, "time")[0]
-    layernames = [f"{b1.name}_{xdim}A", f"{b2.name}_{xdim}B"]
+# def eca_dataset(b1: xr.DataArray, b2: xr.DataArray, b1w: xr.DataArray, b2wr: xr.DataArray, dtype=None, parallel=True):
+#     # TODO: 修正loc A/B的命名
+#     # infer dtype based on the length of time dimension
+#     if dtype is None:
+#         dtype = np.uint8 if b1.shape[0] < 256 else np.uint16 if b1.shape[0] < 65536 else np.uint32  # Cannot use string here in numba
+#     # get the location name from dims
+#     xdim = np.setdiff1d(b1.dims, "time")[0]
+#     layernames = [f"{b1.name}_{xdim}A", f"{b2.name}_{xdim}B"]
 
-    # make sure all DataArray is in ("location", "time") coordinate order if not
-    if b1.dims[0] != 'location':
-        b1 = b1.transpose('location', 'time')
-    if b2.dims[0] != 'location':
-        b2 = b2.transpose('location', 'time')
-    if b1w.dims[0] != 'location':
-        b1w = b1w.transpose('location', 'time')
-    if b2wr.dims[0] != 'location':
-        b2wr = b2wr.transpose('location', 'time')
-    # calculate the ECA
-    if parallel:
-        ECRprec, ECRtrig = eca_parallel(b1.values, b2.values, b1w.values, b2wr.values, dtype=dtype)
-    else:
-        ECRprec, ECRtrig = eca(b1.values, b2.values, b1w.values, b2wr.values, dtype=dtype)
-    # create DataArray
-    coords_locA = b1.indexes['location'].rename(["lat_locA", "lon_locA"])  # 这里一定不能用b1.coords['location'] rename，因为不会作用于MultiIndex
-    coords_locB = b2.indexes['location'].rename(["lat_locB", "lon_locB"])
-    ECRprec = xr.DataArray(ECRprec, coords=[coords_locA, coords_locB], dims=layernames, name="prec_evt",
-                           attrs={'long_name': 'Precursor Events', 'units': 'count', 'dtype': dtype.__name__, 
-                                  'description': 'Number of precursor events (from location A to location B) in location B',
-                                  'eca_params': b1w.attrs["eca_params"]})
-    ECRtrig = xr.DataArray(ECRtrig, coords=[coords_locA, coords_locB], dims=layernames, name="trig_evt",
-                           attrs={'long_name': 'Trigger Events', 'units': 'count', 'dtype': dtype.__name__, 
-                                  'description': 'Number of trigger events (from location A to location B) in location A',
-                                  'eca_params': b2wr.attrs["eca_params"]})
+#     # make sure all DataArray is in ("location", "time") coordinate order if not
+#     if b1.dims[0] != 'location':
+#         b1 = b1.transpose('location', 'time')
+#     if b2.dims[0] != 'location':
+#         b2 = b2.transpose('location', 'time')
+#     if b1w.dims[0] != 'location':
+#         b1w = b1w.transpose('location', 'time')
+#     if b2wr.dims[0] != 'location':
+#         b2wr = b2wr.transpose('location', 'time')
+#     # calculate the ECA
+#     if parallel:
+#         ECRprec, ECRtrig = eca_parallel(b1.values, b2.values, b1w.values, b2wr.values, dtype=dtype)
+#     else:
+#         ECRprec, ECRtrig = eca(b1.values, b2.values, b1w.values, b2wr.values, dtype=dtype)
+#     # create DataArray
+#     coords_locA = b1.indexes['location'].rename(["lat_locA", "lon_locA"])  # 这里一定不能用b1.coords['location'] rename，因为不会作用于MultiIndex
+#     coords_locB = b2.indexes['location'].rename(["lat_locB", "lon_locB"])
+#     ECRprec = xr.DataArray(ECRprec, coords=[coords_locA, coords_locB], dims=layernames, name="prec_evt",
+#                            attrs={'long_name': 'Precursor Events', 'units': 'count', 'dtype': dtype.__name__, 
+#                                   'description': 'Number of precursor events (from location A to location B) in location B',
+#                                   'eca_params': b1w.attrs["eca_params"]})
+#     ECRtrig = xr.DataArray(ECRtrig, coords=[coords_locA, coords_locB], dims=layernames, name="trig_evt",
+#                            attrs={'long_name': 'Trigger Events', 'units': 'count', 'dtype': dtype.__name__, 
+#                                   'description': 'Number of trigger events (from location A to location B) in location A',
+#                                   'eca_params': b2wr.attrs["eca_params"]})
 
-    return ECRprec, ECRtrig
-
-
-def eca_window(b, delt=2, sym=True, tau=0):
-    """
-    b: 一维向量，表示时间序列
-    delt: ECA窗口的大小
-    sym: ECA窗口是否对称
-    tau: ECA窗口的延迟
-
-    return: 
-    bw: 用于计算precursor的窗口
-    bwr: 用于计算trigger的窗口
-
-    note:
-    这里窗口的计算服务于后续与之对应的点的ECA计算, 因此此处不是反映precursor/trigger的对应点, 而是另外一侧
-    """
-    if tau < 0:
-        raise ValueError("tau must be non-negative")
-    window = np.ones((1 + 1 * sym) * delt + 1)
-    # 用于precursor计算的窗口
-    if delt == 0:
-        bw = b  #.copy()
-    else:
-        # bw = np.apply_along_axis(lambda x: np.convolve(x, window)[sym*delt:-delt] >= 0.5, 1, b)
-        bw = (np.convolve(b, window)[sym*delt:-delt] >= 0.5)
-    if tau > 0:
-        bw = np.roll(bw, tau)
-        bw[:, :tau] = False
-
-    # 用于trigger计算的窗口
-    if sym:
-        bwr = bw.copy()
-    else:
-        bwr = np.convolve(b, window)[delt:] >= 0.5
-    if tau > 0:
-        bwr = np.roll(bwr, -tau)
-        bwr[:, -tau:] = False
-
-    return bw, bwr
+#     return ECRprec, ECRtrig
 
 
-def get_eca_window(da: xr.DataArray, delt: int=2, sym: bool=True, tau: int=0) -> xr.DataArray:
-    eca_params = {'delt': delt, 'sym': sym, 'tau': tau}
-    da_prec_window, da_trig_window = xr.apply_ufunc(eca_window, da, input_core_dims=[["time"]], output_core_dims=[["time"], ["time"]], 
-                                        vectorize=True, dask="parallelized", kwargs=eca_params)
-    da_prec_window.attrs = {'long_name': 'Precursor Window', 'units': '1', 'description': 'Window for precursor event identification',
-                            "eca_params": json.dumps(eca_params)}
-    da_trig_window.attrs = {'long_name': 'Trigger Window', 'units': '1', 'description': 'Window for trigger event identification', 
-                            "eca_params": json.dumps(eca_params)}
-    return da_prec_window, da_trig_window
+# def eca_window(b, delt=2, sym=True, tau=0):
+#     """
+#     b: 一维向量，表示时间序列
+#     delt: ECA窗口的大小
+#     sym: ECA窗口是否对称
+#     tau: ECA窗口的延迟
+
+#     return: 
+#     bw: 用于计算precursor的窗口
+#     bwr: 用于计算trigger的窗口
+
+#     note:
+#     这里窗口的计算服务于后续与之对应的点的ECA计算, 因此此处不是反映precursor/trigger的对应点, 而是另外一侧
+#     """
+#     if tau < 0:
+#         raise ValueError("tau must be non-negative")
+#     window = np.ones((1 + 1 * sym) * delt + 1)
+#     # 用于precursor计算的窗口
+#     if delt == 0:
+#         bw = b  #.copy()
+#     else:
+#         # bw = np.apply_along_axis(lambda x: np.convolve(x, window)[sym*delt:-delt] >= 0.5, 1, b)
+#         bw = (np.convolve(b, window)[sym*delt:-delt] >= 0.5)
+#     if tau > 0:
+#         bw = np.roll(bw, tau)
+#         bw[:, :tau] = False
+
+#     # 用于trigger计算的窗口
+#     if sym:
+#         bwr = bw.copy()
+#     else:
+#         bwr = np.convolve(b, window)[delt:] >= 0.5
+#     if tau > 0:
+#         bwr = np.roll(bwr, -tau)
+#         bwr[:, -tau:] = False
+
+#     return bw, bwr
+
+
+# def get_eca_window(da: xr.DataArray, delt: int=2, sym: bool=True, tau: int=0) -> xr.DataArray:
+#     eca_params = {'delt': delt, 'sym': sym, 'tau': tau}
+#     da_prec_window, da_trig_window = xr.apply_ufunc(eca_window, da, input_core_dims=[["time"]], output_core_dims=[["time"], ["time"]], 
+#                                         vectorize=True, dask="parallelized", kwargs=eca_params)
+#     da_prec_window.attrs = {'long_name': 'Precursor Window', 'units': '1', 'description': 'Window for precursor event identification',
+#                             "eca_params": json.dumps(eca_params)}
+#     da_trig_window.attrs = {'long_name': 'Trigger Window', 'units': '1', 'description': 'Window for trigger event identification', 
+#                             "eca_params": json.dumps(eca_params)}
+#     return da_prec_window, da_trig_window
 
 
 """
